5763f8e430893a268e2ad9a43116a360b9967450
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-09 15:18:13 +0200

    modified models to allow different book language

--- littlelibrary/catalog/models.py
+++ littlelibrary/catalog/models.py
@@ -13,6 +13,20 @@ class Genre(models.Model):
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 
 
+#Let's allow a Book to be provided in a different language
+class Language(models.Model):
+    """
+    Model representing a Language (e.g. English, French, Japanese, etc.)
+    """
+    name = models.CharField(max_length=200,
+                            help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
@@ -30,7 +44,8 @@ class Book(models.Model):
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    
     def __str__(self):
         """String for representing the Model object."""
         return self.title
