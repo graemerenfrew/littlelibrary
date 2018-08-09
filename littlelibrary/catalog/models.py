a5794b4127c1a6ebb325c225d444f1232520fbc8
graemerenfrew <graemerenfrew+github@gmail.com>
2018-08-09 16:01:22 +0200

    tidied up the admin pages

--- littlelibrary/catalog/models.py
+++ littlelibrary/catalog/models.py
@@ -10,6 +10,9 @@ class Genre(models.Model):
         return self.name
 
 
+
+
+
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 
 
@@ -45,7 +48,7 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
-    
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -53,6 +56,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 import uuid # Required for unique book instances
 
